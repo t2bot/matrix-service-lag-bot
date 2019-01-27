6d71fea0c96135f54fc0659fb06cf85edf4828e6
Travis Ralston <travpc@gmail.com>
2019-01-26 20:22:48 -0700

    Configure buckets

--- src/metrics.ts
+++ src/metrics.ts
@@ -7,8 +7,7 @@ export const latencyMetric = new client.Histogram({
     name: 't2b_service_latency',
     help: 'Service latency',
     labelNames: ['from', 'to'],
-    //maxAgeSeconds: (config.monitoring.timeout * 2) / 1000,
-    //ageBuckets: 30,
+    buckets: [0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 5.0, 10.0, 15.0]
 });
 
 export const timeoutsMetric = new client.Gauge({
